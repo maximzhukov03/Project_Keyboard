a7ed40d8aac40a8e89ffbc4122df79bb83a47ba7
maxim <maximzhukov03@mail.ru>
2024-10-27 20:28:37 +0300

    star_mod.py

---
+++ GRAF_ANT.py
@@ -0,0 +1,69 @@
+import matplotlib.pyplot as plt
+
+def READ(file):
+    with open(file, "r", encoding="utf-8") as file:
+        text = file.read()
+    return text
+
+RightH = {
+    'f1': set("хцжчкзф8"),
+    'f2': set("уаё6"),
+    'f3': set("яеэ4"),
+    'f4': set("ыьиою02"),
+    'f5': set(" ")
+}
+
+LeftH = {
+    'f1': set("гвц9"),
+    'f2': set("пнй7"),
+    'f3': set("рсш5"),      
+    'f4': set("дтбмл13"),
+}
+
+spec = set(";,?-.:!\"'()\t\n")
+
+LeftHClick = {'f1': 0, 'f2': 0, 'f3': 0, 'f4': 0, 'f5': 0}
+RightHClick = {'f1': 0, 'f2': 0, 'f3': 0, 'f4': 0, 'f5': 0}
+
+def CLICK(txt):
+    for i in txt.lower():
+        if i in spec:
+            continue
+
+        for fin, char in LeftH.items():
+            if i in char:
+                LeftHClick[fin] += 1
+
+        for fin, char in RightH.items():
+            if i in char:
+                RightHClick[fin] += 1
+
+text = READ("voina-i-mir.txt")
+CLICK(text)
+
+print("Левая рука:", LeftHClick)
+print("Правая рука:", RightHClick)
+
+
+fin = ['Миз', 'Безым', 'Сред', 'Указат', 'Пр Больш.']
+LeftHClickGist = [LeftHClick['f1'], LeftHClick['f2'], LeftHClick['f3'], LeftHClick['f4'], LeftHClick['f5']]
+RightHClickGist = [RightHClick['f1'], RightHClick['f2'], RightHClick['f3'], RightHClick['f4'], RightHClick['f5']]
+
+
+fig, os = plt.subplots(figsize=(10, 6))
+BAR_width = 0.4
+index = range(len(fin))
+
+
+os.bar(index, LeftHClickGist, BAR_width, label='Левая рука', color='red')
+os.bar([i + BAR_width for i in index], RightHClickGist, BAR_width, label='Правая рука', color='blue')
+
+os.set_xlabel('Пальцы')
+os.set_ylabel('Кол. нажатий')
+os.set_title('Распределение кликов по пальцам для левой и правой рук (Раскладка ANT)')
+os.set_xticks([i + BAR_width / 2 for i in index])
+os.set_xticklabels(fin)
+os.legend()
+
+plt.tight_layout()
+plt.show()
