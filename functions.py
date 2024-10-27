93aa75dd0b4e3ccb689ece97e095fa7dc4dcdb57
KypezBakaleyhik <whitetiger871@gmail.com>
2024-10-28 02:35:55 +0300

    Add files via upload

--- functions.py
+++ functions.py
@@ -21,10 +21,36 @@ def count_symb(generator):
     shift = 0
     for chunk in generator:
         for char in chunk:
-            if char in alf:
+            if char in alfset:
                 shift += 1
                 char = char.lower()
             if ord(char) in score_symb:
                 score_symb[ord(char)] += 1
     score_symb[0] += shift
     return(score_symb)
+
+"""
+Подсчёт шифтов для уникальных символов 
+"""
+
+def dop_shift(set_shift:set,symbol_strees:dict):
+    shift=0
+    for sym in set_shift:
+        shift+=symbol_strees[sym]
+    return shift
+
+key_stress_ant= dict.fromkeys(ant.values(),0)
+key_stress_qwer= dict.fromkeys(qwerty.values(),0)
+
+def key_stress_counter(key_stress:dict,stress_sumbol:dict,rascl:dict,shift:int):
+    for symb in stress_sumbol.keys():
+        key_stress[rascl[symb]]+=stress_sumbol[symb]
+    key_stress["42"]+=shift
+    return key_stress 
+
+def finger_stress_counter(fin_count:dict,key_stress:dict,main_dict:frozendict):
+    for finger in fin_count.keys():
+       for fingerkeys in main_dict[finger].keys():
+           print(fingerkeys)
+           fin_count[finger] += key_stress[fingerkeys]
+    return fin_count
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from functions import *
+from dicts import *
 shift = 0
-raskl="qwer"
-print("asfasf")
-print(raskl+"alf")
-elem = "А"
-print(shift_checker(elem, qweralf))
-print(elem)
-
-print(shift)
-
+count_symb(read_large_file('d:/Институт/PYTHON/Травов/voina-i-mir.txt', 1024))
+shift_qwer = dop_shift(qwertset, score_symb)
+shift_ant = dop_shift(antset, score_symb)
+print(key_stress_counter(key_stress_qwer, score_symb, qwerty, shift_qwer))
+print(key_stress_counter(key_stress_ant, score_symb, ant, shift_ant))
+print(finger_stress_counter(fin_count_qwer, key_stress_qwer, main_dict))
+print(finger_stress_counter(fin_count_ant, key_stress_ant, main_dict))
